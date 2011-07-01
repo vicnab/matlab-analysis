740e3431c49df91b17629f993c08cedf7a12a529
Benjamin Grant <benjamin.grant.du@gmail.com>
2011-07-01 12:05:55 -0500

    changed directory for finding images for PC debugging, not important but if i don't commit something on git it cries about it next time i try to update so i'm committing meaningless BS. thanks git

--- circle detect/tina_config.m
+++ circle detect/tina_config.m
@@ -1,6 +1,6 @@
 %passdir = uigetdir('C:\Users\Ben\Documents\My Dropbox\Cell Phone Project\Images\10_22_2010');
 if(ispc)
-    passdir = 'C:\Users\benjamingrantdu\Desktop\sample test' ;
+    passdir = 'Z:\Co-workers\Undergraduates\Tina\Assays\06_15_2011\run 4_10 ng';
 elseif(ismac)
     passdir = '/Users/Ben/Dropbox/Colorimetric PSA Detection/Assays/06_28_2011/run 2_10 ng';
 else
@@ -11,7 +11,7 @@ returndir = pwd;
 runname = passdir(end-4:end);
 
 number_tests = 3;
-tb1 = [2 3 4 5];
+tb1 = [2 3 4 5 ];
 tb2 = [7 8 9 10];
 tb3 = [12 13 14 15];
 cb1 = [11 16 17 18 19];
@@ -20,6 +20,6 @@ cb3 = cb1;
 run = Tina_Analysis(passdir, number_tests, tb1, cb1, tb2, cb2, tb3, cb3);
 save([passdir '/' runname '.mat'], 'run');
 clear run;
-testnames = {'P20C', 'A06', 'PC80'};
+testnames = {'M167', 'P20C', '5G6'};
 Tina_Single_Run_Analysis(testnames, runname, passdir);
 cd(returndir);
